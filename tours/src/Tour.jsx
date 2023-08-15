a584930e93b12e67735819e36d38dc9e1d5b3337
Zakhar Lobai <lobajzahar@gmail.com>
2023-08-16 00:54:00 +0200

    tours projec

---
+++ tours/src/Tour.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, image, name, info, price, removeTour }) => {
+    const [readMore, setReadMore] = useState(false);
+    return (
+        <article className="single-tour">
+            <img src={image} alt={name} className="img" />
+            <span className="tour-price">${price}</span>
+            <div className="tour-info">
+                <h5>{name}</h5>
+
+                <p>{readMore ? info : `${info.substring(0, 200)}...`}
+                <button className="info-btn" onClick={() => setReadMore(!readMore)}>
+                    {readMore ? 'show less' : ' read more'}
+                </button>
+                </p>
+
+                <button className="delete-btn btn-block btn">not interested</button>
+            </div>
+        </article>
+    );
+};
+
+export default Tour;
